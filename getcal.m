e77918bb0d29889e70fb87def20fb952f49e4117
Oliver Thomas Lord <oliver.lord@bristol.ac.uk>
2017-10-23 22:52:36 +0100

    Remove deleted files

--- getcal.m
+++
@@ -1,6 +0,0 @@
-kp = get(a11, 'String'); conp=eval(kp); %constant
-p1 = get(a12, 'String'); pix1=eval(p1); %parameter 1
-p2 = get(a13, 'String'); pix2=eval(p2); %parameter2
-kl = get(a14, 'String'); conl=eval(kl); %constant
-l1 = get(a15, 'String'); lam1=eval(l1); %parameter 1
-l2 = get(a16, 'String'); lam2=eval(l2); %parameter2
