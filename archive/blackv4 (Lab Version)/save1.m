9ea3ba13e63834c5ffa375fdd52f2f732cf282c1
Oliver Lord <oliverlord@it032120.users.bris.ac.uk>
2019-02-21 16:17:25 +0000

    removed archive from remote repo.

--- archive/blackv4 (Lab Version)/save1.m
+++
@@ -1,9 +0,0 @@
-[sfile1,spath]=uiputfile('.dat','Wien or Planck Fits ASCII file');
-
-savefile1=strcat(spath,sfile1);
-
-if (ff==1) sd1=[omega(lpix:hpix)',j(lpix:hpix,mnrow:mxrow)];end;
-
-if (ff==2) sd1=[w(1,lpix:hpix)',nincal(lpix:hpix,mnrow:mxrow)];end;
-
-dlmwrite(savefile1,sd1,'\t');
--- archive/blackv4 (Office Version)/save3.m
+++
@@ -1,9 +0,0 @@
-[sfile3,spath3]=uiputfile('.dat','Temperature Error Data ASCII file');
-
-savefile3=strcat(spath3,sfile3);
-
-if (em==0) sd3=[xrange(mnrow:mxrow)',delta(mnrow:mxrow)'];end;
-
-if (em>0) sd3=[xrange(mnrow:mxrow)',deltec(amc,mnrow:mxrow)'];end;
-
-dlmwrite(savefile3,sd3,'\t');
